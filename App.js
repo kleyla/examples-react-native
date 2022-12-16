b0d358a9a0a5e31cfd20809443dd528064660199
karen.rodriguez <karen.rodriguez@bg.com.bo>
2022-12-16 19:26:56 -0400

    first com

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { styles } from "./src/styles";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hola!</Text>
+      <MyPrimerComponente name={"Karen"} carrera={"sis"} />
+      <MyPrimerComponente name={"Diana"} />
+      <MyPrimerComponente name={"Jorge"} />
+      <Card>
+        <Text>Contenido hijo</Text>
+      </Card>
     </View>
   );
 }
+// props
+const MyPrimerComponente = ({ name, carrera = "Redes" }) => {
+  return (
+    <View style={styles.misEstilos}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Hola {name}!</Text>
+      <Text>Carrera {carrera} !</Text>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Card = ({ children }) => {
+  return <View style={styles.card}>{children}</View>;
+};
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  misEstilos: {
+    backgroundColor: "red",
+  },
+  card: {
+    padding: 12,
+    backgroundColor: "yellow",
+  },
+});
